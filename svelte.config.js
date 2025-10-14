23c17b65cb85e27780284ea20ec570752f2038e5
Jonathan G. Hecl <gshaxor@gmail.com>
2025-10-14 03:14:33 -0300

    chore: switch from Cloudflare to static adapter with build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    })
   }
 };
-
 export default config;
